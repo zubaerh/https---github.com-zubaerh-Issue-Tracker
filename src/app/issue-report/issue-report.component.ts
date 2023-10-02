72f611d9a6324961f85065b6194128022e97343d
ASUS <zubbaerhossain@gmail.com>
2023-10-02 20:20:59 +0600

    1st commit

---
+++ src/app/issue-report/issue-report.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IssuesService } from '../issues.service';
+import { Issue } from '../issue';
+
+@Component({
+  selector: 'app-issue-report',
+  templateUrl: './issue-report.component.html',
+  styleUrls: ['./issue-report.component.css']
+})
+export class IssueReportComponent implements OnInit {
+  @Output() formClose = new EventEmitter();
+  issueForm: FormGroup | undefined;
+  suggestions: Issue[]= [];
+  constructor( private builder: FormBuilder , private issueService: IssuesService ){
+  }
+  ngOnInit(): void {
+    this.issueForm = this.builder.group({
+      title: ['', Validators.required],
+      description: [''],
+      priority: ['', Validators.required],
+      type: ['', Validators.required]
+      });
+      this.issueForm.controls['title'].valueChanges.subscribe((
+        title: string) => {
+        this.suggestions = 
+        this.issueService.getSuggestions(title);
+        });
+  }
+  addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
+      this.issueForm.markAllAsTouched();
+      return;
+      }
+    this.issueService.createIssue(this.issueForm?.value);
+    this.formClose.emit();
+   }
+
+}
